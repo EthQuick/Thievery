35d9025691dc1d720381d96937f0fc3935ee8e23
Ethan Quick <efquick97@gmail.com>
2018-10-02 14:56:30 -0700

    Added stealth and the beginnings of loot

---
+++ Assets/Coin.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+func _physics_process(delta):
+	#TODO increment the loot variable in player when it intersects with this
--- Stages/Player.gd
+++ Stages/Player.gd
@@ -1,9 +1,10 @@
 extends KinematicBody2D
 
 var motion = Vector2()
-const MAX_SPEED = 100
+var stealth = false
+var loot = 0
+var MAX_SPEED = 100
 const ACCELERATION = 25
-
 
 func _physics_process(delta):
 	
@@ -19,6 +20,16 @@ func _physics_process(delta):
 		motion.y = min(motion.y + ACCELERATION, MAX_SPEED)
 	else:
 		motion.y = lerp(motion.y, 0, 0.2)
+	
+	if Input.is_action_just_pressed("ui_select"):
+		stealth = not stealth
+	
+	if stealth:
+		$Sprite.play("Stealth_Idle")
+		MAX_SPEED = 50
+	else:
+		$Sprite.play("Unstealth_Idle")
+		MAX_SPEED = 100
 	
 	move_and_slide(motion)
 	pass
